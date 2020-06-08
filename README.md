93a7ceee7bc27da683b5bbe3f3ec35be32b99cec
ulala8725 <shawna2587@gmail.com>
2020-06-08 23:57:59 +0900

    add html

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # test_wetube
 cloning Youtube with Vanilla and NodeJS
+
+## Pages:
+
+- [ ] Home
+- [ ] Join
+- [ ] Login
+- [ ] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,8 +1,19 @@
-//export const home = (req, res) => res.render('home', { pageTitle : 'Home' }); 
-export const home = (req, res) => res.render('home', { pageTitle : 'Home'); 
-export const search = (req, res) => res.send('search', { pageTitle : 'Search' });
-export const videos = (req, res) => res.send('videos', { pageTitle : 'Videos' });
-export const upload = (req, res) => res.send('upload', { pageTitle : 'Upload' });
-export const videoDetail = (req, res) => res.send('videoDetail', { pageTitle : 'Video Detail' });
-export const editVideo = (req, res) => res.send('editVideo', { pageTitle : 'Edit Video' });
-export const deleteVideo = (req, res) => res.send('deleteVideo', { pageTitle : 'Delete Video' });
+export const home = (req, res) => res.render('home', { pageTitle : 'Home'}); 
+
+export const search = (req, res) => {
+    // const searchingBy = req.query.term (ES5)
+    // "term"라는 name을 가진 form의 입력값을 
+    // "searchingBy"라는 변수명으로 받는다.
+    const {query: { term: searchingBy }} = req; // (ES6)
+    res.render('search', { pageTitle : 'Search', searchingBy })
+};
+
+export const videos = (req, res) => res.render('videos', { pageTitle : 'Videos' });
+
+export const upload = (req, res) => res.render('upload', { pageTitle : 'Upload' });
+
+export const videoDetail = (req, res) => res.render('videoDetail', { pageTitle : 'Video Detail' });
+
+export const editVideo = (req, res) => res.render('editVideo', { pageTitle : 'Edit Video' });
+
+export const deleteVideo = (req, res) => res.render('deleteVideo', { pageTitle : 'Delete Video' });
